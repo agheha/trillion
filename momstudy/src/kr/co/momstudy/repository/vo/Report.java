b7174406b4765f9357a13d1fd6553d22992186c7
ToysRose <whahfwn2@naver.com>
2019-09-27 19:26:38 +0900

    관리자
    mybatis 환경설정중

---
+++ momstudy/src/kr/co/momstudy/repository/vo/Report.java
@@ -0,0 +1,75 @@
+package kr.co.momstudy.repository.vo;
+
+import java.util.Date;
+
+public class Report {
+	
+	private int num;
+	private String type;
+	private String reportTarget;
+	private int boardNum;
+	private String content;
+	private int code;
+	private String email;
+	private Date regDate;
+
+//	조인해서 가져온 결과를 받기위함
+	private String reseaon;
+	public String getReseaon() {
+		return reseaon;
+	}
+	public void setReseaon(String reseaon) {
+		this.reseaon = reseaon;
+	}
+	
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getReportTarget() {
+		return reportTarget;
+	}
+	public void setReportTarget(String reportTarget) {
+		this.reportTarget = reportTarget;
+	}
+	public int getBoardNum() {
+		return boardNum;
+	}
+	public void setBoardNum(int boardNum) {
+		this.boardNum = boardNum;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	
+}
