1df98c5e8dff895008eb2326bf1125f147dc6596
kumashin7 <gomshin777@gmail.com>
2019-09-27 18:20:57 +0900

    아이디 찾기, 비밀번호 찾기(진행중)

--- momstudy/src/kr/co/momstudy/user/controller/FindPasswordController.java
+++ momstudy/src/kr/co/momstudy/user/controller/FindPasswordController.java
@@ -1,10 +1,18 @@
 package kr.co.momstudy.user.controller;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.mail.HtmlEmail;
 
 import kr.co.momstudy.common.db.MyAppSqlConfig;
 import kr.co.momstudy.repository.dao.UserDAO;
+import kr.co.momstudy.repository.vo.User;
 
 @WebServlet("/user/findpass.do")
 public class FindPasswordController extends HttpServlet{
@@ -13,4 +21,21 @@ public class FindPasswordController extends HttpServlet{
 	public FindPasswordController() {
 		this.dao = MyAppSqlConfig.getSqlSessionInstance().getMapper(UserDAO.class);
 	}
+	
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		User u = new User();
+		u.setName(req.getParameter("name"));
+		u.setEmail(req.getParameter("email"));
+		
+		User  user = dao.searchPass(u);
+		if (user == null) {
+			user = null;
+		}
+	}
+	
+	
+	HtmlEmail mail = new HtmlEmail();
+	
 }
