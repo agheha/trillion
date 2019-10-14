e31f64fcd262c6cc2a77b41ccdc0927eab5606c1
Kumashin <gomshin777@gmail.com>
2019-10-14 21:48:34 +0900

    스터디 탈퇴 모달 창 및 버튼 추가

--- momstudy/WebContent/jsp/user/deleteagree.jsp
+++ momstudy/WebContent/jsp/user/deleteagree.jsp
@@ -12,28 +12,5 @@
     </div>
 	
 	
-    <script>
-    	function agree() {
-    		let divEle =  document.querySelector("#content");
-    		let cancells = document.querySelectorAll("input[name='cancell']")
-    		console.dir(cancells)
-    		divEle.innerHTML = "<div id='que'>탈퇴되었습니다</div>"
-    		cancells = JSON.stringify(cancells)
-    		let xhr = new XMLHttpRequest();
-    		xhr.onreadystatechange = e => {
-    			if (xhr.readyState === 4) {
-    				if (xhr.status === 200) {
-    					setTimeout(() => {
-    						location.href="/momstudy/user/studydeleteform.do"
-    					}, 2000);
-    				} 
-    			}
-    		}
-    		xhr.open("POST", "/momstudy/user/studydelete.do")
-    		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    		xhr.send("cancell="+cancells);
-  			
-    		
-    	}
-    </script>
+    <script src="<c:url value="/script/user/deletestudy.js"/>"></script>	
     <script src="<c:url value="/script/study/studysignup.js"/>"></script>		
--- momstudy/src/kr/co/momstudy/user/controller/StudyDeleteContorller.java
+++ momstudy/src/kr/co/momstudy/user/controller/StudyDeleteContorller.java
@@ -1,6 +1,8 @@
 package kr.co.momstudy.user.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,8 +10,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
 import kr.co.momstudy.common.db.MyAppSqlConfig;
 import kr.co.momstudy.repository.dao.UserDAO;
+import kr.co.momstudy.repository.vo.Participant;
+import kr.co.momstudy.repository.vo.User;
 
 
 @WebServlet("/user/studydelete.do")
@@ -20,6 +28,12 @@ public class StudyDeleteContorller extends HttpServlet {
 	}
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		System.out.println(req.getParameter("cancell"));
+		User user = (User)req.getSession().getAttribute("user");
+		List<Participant> list = new ArrayList<>();
+		Participant part = new Participant();
+		part.setEmail(user.getEmail());
+		part.setNum(Integer.parseInt(req.getParameter("cancell")));
+		dao.deleteStudy(part);
 	}
+	
 }
