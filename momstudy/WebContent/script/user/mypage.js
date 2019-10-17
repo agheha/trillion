b5c3f74f56e2b8b1bdcba483110f898ec5f6f7f6
thxtome <thxtome531@gmail.com>
2019-10-17 18:18:57 +0900

    마이페이지 화면 수정
    화살표 조건 추가

--- momstudy/WebContent/script/user/mypage.js
+++ momstudy/WebContent/script/user/mypage.js
@@ -3,48 +3,51 @@ let xindex = 0;
 let sForm = document.sForm;
 let numput = document.querySelector("#numput")
 
-function slidePrev(){
-	xindex = xindex + 270;
-	forslide.style.left = xindex + 'px';
-	if(xindex >= 270) {
-		setTimeout(()=>{
-			xindex = xindex - 270;
-			forslide.style.left = xindex + 'px';
-		}, 100);
-		return;
+if(document.querySelector("#forslide") !== null){
+	let forslide = document.querySelector(".forslide");
+	let cardEle = document.querySelectorAll(".studycard");
+	forslide.style.width = cardEle.length * 270 + 'px';	
+	cardEle.forEach((card)=>{
+		card.addEventListener("click",()=>{
+			numput.value = parseInt(card.getAttribute("num"));
+			sForm.submit();
+		})
+	})
+	
+	function slidePrev(){
+		xindex = xindex + 270;
+		forslide.style.left = xindex + 'px';
+		if(xindex >= 270) {
+			setTimeout(()=>{
+				xindex = xindex - 270;
+				forslide.style.left = xindex + 'px';
+			}, 100);
+			return;
+		}
 	}
-}
 
-function slideNext(){
-	xindex = xindex - 270;
-	forslide.style.left = xindex + 'px';
-	if(xindex < parseInt(forslide.style.width) * -1 + 270 * 4) {
-        setTimeout(()=>{
-        	xindex = xindex + 270;
-        	forslide.style.left = xindex + 'px';
-        }, 100);
-        return;
-    }
-}
+	function slideNext(){
+		xindex = xindex - 270;
+		forslide.style.left = xindex + 'px';
+		if(xindex < parseInt(forslide.style.width) * -1 + 270 * 4) {
+	        setTimeout(()=>{
+	        	xindex = xindex + 270;
+	        	forslide.style.left = xindex + 'px';
+	        }, 100);
+	        return;
+	    }
+	}
+	
+	slidePrev();
 
-let forslide = document.querySelector(".forslide");
-let cardEle = document.querySelectorAll(".studycard");
-forslide.style.width = cardEle.length * 270 + 'px';
+}
 
 document.querySelector("#prevBtn").addEventListener("click",slidePrev);
 document.querySelector("#nextBtn").addEventListener("click",slideNext);
 
-if(document.querySelector("#smallcon") !== null){
+if(document.querySelector(".smallcon") !== null){
 	document.querySelector("#prevBtn").remove();
 	document.querySelector("#nextBtn").remove();
 }
 
 
-cardEle.forEach((card)=>{
-	card.addEventListener("click",()=>{
-		numput.value = parseInt(card.getAttribute("num"));
-		sForm.submit();
-	})
-})
-
-slidePrev();
